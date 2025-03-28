9b6dc81c45256c53e2d25663d947b47f7a8ebd23
Juan Melendres <juan.mele97@gmail.com>
2025-03-27 22:27:42 -0600

    Add complete Challenge

---
+++ ecommerce-api/src/test/java/com/javm/ecommerceapi/EcommerceApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.javm.ecommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
